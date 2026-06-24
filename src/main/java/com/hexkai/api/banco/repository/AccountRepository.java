3d08f378da28d4286bed3413ccd5e6c9f4c96b69
lins07 <estudoslins69@gmail.com>
2026-06-24 09:00:10 -0300

    feat: adicionar interfaces UserRepository e AccountRepository

--- src/main/java/com/hexkai/api/banco/repository/AccountRepository.java
+++ src/main/java/com/hexkai/api/banco/repository/AccountRepository.java
@@ -1,8 +1,9 @@
 package com.hexkai.api.banco.repository;
 
-import com.hexkai.api.banco.domain.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.hexkai.api.banco.domain.models.Account;
 
 import java.util.Optional;
 import java.util.UUID;
--- src/main/java/com/hexkai/api/banco/repository/UserRepository.java
+++ src/main/java/com/hexkai/api/banco/repository/UserRepository.java
@@ -1,8 +1,9 @@
 package com.hexkai.api.banco.repository;
 
-import com.hexkai.api.banco.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.hexkai.api.banco.domain.models.User;
 
 import java.util.Optional;
 import java.util.UUID;
